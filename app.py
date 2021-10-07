9ea603a940cc0ce636e244df008d6b3788625106
Anderson Araújo <81644687+Andssu@users.noreply.github.com>
2021-10-06 22:33:02 -0300

    adicionado modulo 3 e fim de cadastro de filtros para o jinja

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
 from flask import Flask, render_template, flash
+from flask_bootstrap import Bootstrap
+from datetime import datetime
 
+#IMPORTANDO OS FILTROS!!
+import filtro
+
+
+
+#outra flag pra a classe Flask pode ser static_folder='nomedapasta' por padrao, é static
 app = Flask(__name__, template_folder='views')
 app.config['SECRET_KEY'] = 'dev'
 
+
+#CADASTRANDO FILTROS NO FLASK
+#a chave é o nome da funcao dentro do html e o valor é a funcao
+app.jinja_env.filters['format_dat'] = filtro.formatar_data
+app.jinja_env.filters['teste'] = filtro.teste
+app.jinja_env.filters['f3'] = filtro.func3
 @app.route('/')
 def index():
-    user = {
-        'name': 'marcos souza junior',
-        'idade': 10000,
-        'email': 'sousajunior@gmail.com'
-    }
+
     #('.html', user = user) o primeiro user é como será chamado dentro do template, e o segundo é o objeto atual da funcao que será passado
     flash('Mensagem que vou enviar' )
     flash('massavelio')
-    return render_template('index.html', user=user)
+    return render_template('index.html', users=users)
 
+@app.route('/users')
+def users():
+    usuariosss = [{
+        'nome': 'marcos souza junior',
+        'idade': 10000,
+        'email': 'sousajunior@gmail.com',
+        'status': True,
+        'since': datetime.utcnow()
+    }, {
+        'nome': 'mahria Joselina',
+        'idade': 20002020,
+        'email': 'joseina20000000 A.C @gmail.com',
+        'status': False,
+        'since': datetime.utcnow()
+    }
+    ]
+    return render_template('users.html', users=usuariosss)
+
+@app.route('/condicionaiss')
+def condi():
+    up = True
+    st = 'este é um teste de mensagem.'
+    return render_template('condicionais.html', user_page=up, texto=st)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ filtro.py
@@ -0,0 +1,8 @@
+def formatar_data(valor):
+    return valor.strftime("%d-%m-%Y %H:%M:%S")
+
+def teste(v, s='TEXTO PADRAO'):
+    return str(v) + ' inserido na funcao python ' + str(s)
+
+def func3(v,s,t):
+    return v + ' | ' + s + ' | ' + t
