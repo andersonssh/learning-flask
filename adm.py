898945d562dcff0309b18b4ba8e17e3c9a95af7e
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-11-03 08:13:58 -0300

    Adiciona modulo de flask admin
    -adiciona modelos ao painel adm
    -adiciona configuracoes de modelos personalizados dentro do painel adm
    (analisar codigo fonte)

--- adm.py
+++
@@ -1,6 +0,0 @@
-from models import User
-from app import db
-from flask_admin.contrib.sqla import ModelView
-
-def init_app(admin):
-    admin.add_view(ModelView(User, db.session))
--- progresso/parte11 - flask-admin/app.py
+++ progresso/parte11 - flask-admin/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'login.html'
 
 def create_app():
     app = Flask(__name__)
@@ -12,12 +16,11 @@ def create_app():
 
     #passando controlador do sqlalchemy para app
     db.init_app(app)
-
     #area adm
     import adm
     admin = Admin(app, name='Andssu trol', template_mode='bootstrap4')
     adm.init_app(admin)
-
+    login_manager.init_app(app)
     return app
 
 if __name__ == '__main__':
