288b719eb64bdf76bd56e03258d9fe5d10d0201d
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-11-05 07:42:48 -0300

    adiciona modulo - manipulando bd com sqlalcemy

---
+++ utils.py
@@ -0,0 +1,25 @@
+from models import Pessoas
+
+def insere_pessoas():
+    pessoa = Pessoas(nome='Luks', idade='29')
+    pessoa.save()
+
+def consulta_pessoas():
+    pessoa = Pessoas.query.all()
+    pessoa = Pessoas.query.filter_by(nome='Joaozao').first()
+    print(pessoa.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Joaozao').first()
+    pessoa.idade = 21
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Joaozao")
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    exclui_pessoa()
+    altera_pessoa()
+    consulta_pessoas()
