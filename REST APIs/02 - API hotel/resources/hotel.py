f52fff83ffef63ee1005da82156c44ed74858622
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-12-20 10:25:47 -0300

    Adiciona put e refatora post e get

--- REST APIs/02 - API hotel/resources/hotel.py
+++ REST APIs/02 - API hotel/resources/hotel.py
@@ -28,28 +28,43 @@ class Hoteis(Resource):
 
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    # pegar apenas os argumentos definidos abaixo
+    argumentos.add_argument('nome', required=True, help='O nome nao pode ser vazio!')
+    argumentos.add_argument('estrelas', type=float)
+    argumentos.add_argument('diaria', type=float)
+    argumentos.add_argument('cidade')
+
+    def encontrar_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
 
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.encontrar_hotel(hotel_id)
+
+        if hotel:
+            return hotel
+
         return {'message': 'Hotel not found'}, 404
 
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        # pegar apenas os argumentos definidos abaixo
-        argumentos.add_argument('nome', required=True, help='O nome nao pode ser vazio!')
-        argumentos.add_argument('estrelas', type=float)
-        argumentos.add_argument('diaria', type=float)
-        argumentos.add_argument('cidade')
-
-        dados = {'hotel_id': hotel_id, **argumentos.parse_args()}
+        dados = {'hotel_id': hotel_id, **Hotel.argumentos.parse_args()}
         hoteis.append(dados)
         return dados
 
 
     def put(self, hotel_id):
-        pass
+        hotel = Hotel.encontrar_hotel(hotel_id)
+        novo_hotel = {'hotel_id': hotel_id, **Hotel.argumentos.parse_args()}
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200, 'OK'
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 # codigo para: created
 
     def delete(self, hotel_id):
         pass
