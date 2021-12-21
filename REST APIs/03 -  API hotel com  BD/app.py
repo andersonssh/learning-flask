e078c3ff332202599927ab1a7960c2f606cfe8e4
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-12-21 16:11:00 -0300

    Adiciona configuracoes para o bd sqlalchemy

--- REST APIs/03 -  API hotel com  BD/app.py
+++ REST APIs/03 -  API hotel com  BD/app.py
@@ -5,8 +5,18 @@ from resources.hotel import Hoteis, Hotel
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.before_first_request
+def cria_banco():
+    bd.create_all()
+
+
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
+    from sqlalchemy_ import bd
+    bd.init_app(app)
     app.run(debug=True)
--- REST APIs/03 -  API hotel com  BD/models/hotel.py
+++ REST APIs/03 -  API hotel com  BD/models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel:
+from sqlalchemy_ import bd
+
+class HotelModel(bd.Model):
+    __tablename__ = 'hoteis'
+    hotel_id = bd.Column(bd.String, primary_key=True)
+    nome = bd.Column(bd.String(80))
+    estrelas = bd.Column(bd.Float(precision=1))
+    diaria = bd.Column(bd.Float(precision=2))
+    cidade = bd.Column(bd.String(40))
+
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
