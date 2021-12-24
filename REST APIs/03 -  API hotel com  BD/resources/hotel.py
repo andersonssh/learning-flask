6e4bce2fae085a0c805ef577e2f0eeb9db913818
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-12-23 22:49:42 -0300

    Remove variavel de hoteis

--- REST APIs/03 -  API hotel com  BD/resources/hotel.py
+++ REST APIs/03 -  API hotel com  BD/resources/hotel.py
@@ -1,28 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-        'hotel_id': 'xx',
-        'nome': 'Hotelzao 1',
-        'estrelas': 4.3,
-        'diaria': 420.34,
-        'cidade': 'RJ'
-    },{
-        'hotel_id': 'yy',
-        'nome': 'Hotelzao 3',
-        'estrelas': 4.8,
-        'diaria': 770.34,
-        'cidade': 'SC'
-    },{
-        'hotel_id': 'zz',
-        'nome': 'Hotelzao 3',
-        'estrelas': 3.8,
-        'diaria': 500.34,
-        'cidade': 'SP'
-    },
-]
-
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
