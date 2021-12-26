d871e96223bb3a37ce2a6e2fb65c09b2da4d4cae
andersonssh <4nd3r50n4r4uj0@gmail.com>
2021-12-26 09:50:39 -0300

    Separa usuario de hotel em arquivos diferentes

--- REST APIs/03 -  API hotel com  BD/models/hotel.py
+++ REST APIs/03 -  API hotel com  BD/models/hotel.py
@@ -50,39 +50,3 @@ class HotelModel(bd.Model):
         bd.session.commit()
 
 
-class UserModel(bd.Model):
-    __tablename__ = 'usuarios'
-    user_id = bd.Column(bd.Integer, primary_key=True)
-    login = bd.Column(bd.String(40))
-    senha = bd.Column(bd.String(40))
-
-    def __init__(self, login, senha):
-        self.login = login
-        self.senha = senha
-
-    def json(self):
-        return {
-            'user_id': self.user_id,
-            'login': self.login,
-        }
-
-    @classmethod
-    def encontra_hotel(cls, user_id):
-        user = cls.query.get(user_id)
-        if user:
-            return user
-        return None
-
-    def save_user(self):
-        bd.session.add(self)
-        bd.session.commit()
-
-    def update_user(self, login, senha):
-        self.login = login
-        self.senha = senha
-
-        self.save_user()
-
-    def delete_user(self):
-        bd.session.delete(self)
-        bd.session.commit()
